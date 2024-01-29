c60dc22a79e25287d15949b77996981ec5bf6a7f
JeongMoJin <wlswjdah123@naver.com>
2024-01-29 23:05:21 +0900

    2024-01-29

--- app/src/main/java/com/example/fastcampus/ActivityTwo.kt
+++ app/src/main/java/com/example/fastcampus/ActivityTwo.kt
@@ -1,11 +1,24 @@
 package com.example.fastcampus
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class ActivityTwo : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_two)
+        setContentView(R.layout.activity_two2)
+
+        (findViewById<TextView>(R.id.one)).setOnClickListener {
+            startActivity(Intent(this@ActivityTwo, ActivityOne::class.java))
+        }
+        (findViewById<TextView>(R.id.two)).setOnClickListener {
+            startActivity(Intent(this@ActivityTwo, ActivityTwo::class.java))
+        }
+        (findViewById<TextView>(R.id.three)).setOnClickListener {
+            startActivity(Intent(this@ActivityTwo, ActivityThree::class.java))
+        }
+
     }
 }
