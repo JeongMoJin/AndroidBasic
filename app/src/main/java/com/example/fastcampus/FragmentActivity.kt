194366aefaf0d7436c4cb0f4f0afae3ffb0dfe47
JeongMoJin <wlswjdah123@naver.com>
2024-01-31 20:31:13 +0900

    2024-01-31

--- app/src/main/java/com/example/fastcampus/FragmentActivity.kt
+++ app/src/main/java/com/example/fastcampus/FragmentActivity.kt
@@ -2,6 +2,7 @@ package com.example.fastcampus
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
 
 class FragmentActivity : AppCompatActivity() {
@@ -15,15 +16,6 @@ class FragmentActivity : AppCompatActivity() {
         // Transaction
         // - 작업의 단위 -> 시작과 끝이 있다
         // A, B, C, D
-        (findViewById<TextView>(R.id.add)).setOnClickListener {
-            val transaction = fragmentManager.beginTransaction() // 시작
-            transaction.replace(R.id.root, fragmentFirst)
-            transaction.commit() // 끝
-        }
-
-        (findViewById<TextView>(R.id.add)).setOnClickListener {
-
-        }
         // Commit
         // 1> commit
         // 2> commiAllowingStateLoss
@@ -32,7 +24,46 @@ class FragmentActivity : AppCompatActivity() {
         // - 상태손실을 허락한다
         // - onStop, lifecycle 또는 os에 의해서 앱이 상태를 저장해야 할 수 있다
         // - 상태저장 : onSaveInstanceState
+        // - commit : 저장을 한 경우 실행 할 수 없다 (IllegalStateException)
+        // - commitAllowingStateLoss : 저장을 한 경우 예외가 발생하지않고 그냥 손실
         // commit - commitNow(Now)
+        // - commit -> 작업을 예약한다 (메인 쓰레드에 예약이 된다)
+        // - commitNow -> 바로 실행해!
+
+        (findViewById<TextView>(R.id.add)).setOnClickListener {
+            val transaction = fragmentManager.beginTransaction() // 시작
+            // 프래그먼트에 데이터를 전달 하는 방법
+            val bundle = Bundle()
+            bundle.putString("key", "hello")
+            fragmentFirst.arguments = bundle
+
+
+            transaction.replace(R.id.root, fragmentFirst, "fragment_first_tag")
+            transaction.commit() // 끝
+        }
+
+        (findViewById<TextView>(R.id.remove)).setOnClickListener {
+            val transaction = fragmentManager.beginTransaction()
+            transaction.remove(fragmentFirst)
+            transaction.commit()
+        }
+
+        (findViewById<TextView>(R.id.access_fragment)).setOnClickListener {
+            // XML에 있는 fragment를 찾는 방법
+//            val firstFragment =
+//                supportFragmentManager.findFragmentById(R.id.fragment_first) as FragmentFirst
+//            fragmentFirst.printTestLog()
+            // XML에 없는 fragment를 찾는 방법
+            supportFragmentManager.findFragmentByTag("fragment_first_tag") as FragmentFirst
+            fragmentFirst.printTestLog()
+        }
+
 
     }
+
+    fun printTestLog() {
+        TODO("Not yet implemented")
+    }
+
+
 }
