18ebc27a837b65e8d8863d9d197c027281d8b780
JeongMoJin <wlswjdah123@naver.com>
2024-02-13 22:45:48 +0900

    2024-02-13

--- app/src/main/java/com/example/fastcampus/YoutubeActivity.kt
+++ app/src/main/java/com/example/fastcampus/YoutubeActivity.kt
@@ -1,12 +1,22 @@
 package com.example.fastcampus
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.zip.Inflater
 
 class YoutubeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,11 +34,78 @@ class YoutubeActivity : AppCompatActivity() {
                 call: Call<ArrayList<YoutubeItem>>,
                 response: Response<ArrayList<YoutubeItem>>
             ) {
+                val youtubeItemList = response.body()
+                val adapter = YoutubeListAdapter(
+                    youtubeItemList!!,
+                    LayoutInflater.from(this@YoutubeActivity),
+                    this@YoutubeActivity
+                )
             }
 
             override fun onFailure(call: Call<ArrayList<YoutubeItem>>, t: Throwable) {
+                Log.d("youyou", "fail" + t.message)
             }
         })
-
     }
 }
+
+class YoutubeListAdapter(
+    val youtubeItemList : ArrayList<YoutubeItem>,
+    val inflater: LayoutInflater,
+    val contenxt : Context
+): RecyclerView.Adapter<YoutubeListAdapter.ViewHolder>(){
+
+    inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+        val title : TextView
+        val thumnail : ImageView
+        val content : TextView
+
+        init {
+            title = itemView.findViewById(R.id.title)
+            thumnail = itemView.findViewById(R.id.thumbnail)
+            content = itemView.findViewById(R.id.content)
+
+            itemView.setOnClickListener{
+                val position: Int = adapterPosition
+                val intent = Intent(contenxt, YoutubeItemActivity::class.java)
+                contenxt.startActivity(intent)
+            }
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = inflater.inflate(R.layout.youtube_list_item, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.title.text = youtubeItemList.get(position).title
+        holder.content.text = youtubeItemList.get(position).content
+    }
+
+    override fun getItemCount(): Int {
+        return youtubeItemList.size
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
